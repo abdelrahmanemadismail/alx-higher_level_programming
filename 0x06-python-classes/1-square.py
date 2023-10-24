bcab3a90cee15cfdb8ea84983491ba1b05566216
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-10-24 12:10:59 -0700

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square that represents a square.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
