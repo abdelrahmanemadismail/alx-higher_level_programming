3d0bb4767a059376a9181d3f18cc1020422cbb03
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-10-24 12:37:05 -0700

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square that represents a square.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square (default is 0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size attribute.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size attribute.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
