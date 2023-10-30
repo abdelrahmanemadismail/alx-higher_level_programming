4e43024cbd144616fbf6d68e41a1b4154c0a249c
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-10-30 11:48:07 -0700

    Create 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle that represents a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+    """
+    pass
