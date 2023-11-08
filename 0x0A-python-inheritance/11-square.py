e7ea246d99b3bd45f10c4fd824b680f3d07d240f
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-11-08 14:41:37 -0800

    Create 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module for Square class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class for representing squares, inheriting from Rectangle.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance.
+        __str__(self): Provides  custom string representation of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Provides a custom string representation of the square.
+
+        Returns:
+            str: A string representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__width, self.__height)
