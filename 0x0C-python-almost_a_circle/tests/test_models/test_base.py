1671556108d1cb58c5da95f617dd133dedcb1f0a
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2024-01-15 10:34:28 -0800

    Almost a Circle - 15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -16,6 +16,25 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
 
+    def test_to_json_string(self):
+        """ Test to_json_string method """
+        # Test with a non-empty list of dictionaries
+        list_of_dicts = [{"id": 1, "x": 2, "y": 3}, {"id": 2, "x": 4, "y": 5}]
+        json_string = Base.to_json_string(list_of_dicts)
+        self.assertEqual(
+                json_string,
+                '[{"id": 1, "x": 2, "y": 3}, {"id": 2, "x": 4, "y": 5}]'
+                )
+
+        # Test with an empty list
+        empty_list = []
+        json_string_empty = Base.to_json_string(empty_list)
+        self.assertEqual(json_string_empty, '[]')
+
+        # Test with None
+        json_string_none = Base.to_json_string(None)
+        self.assertEqual(json_string_none, '[]')
+
 
 if __name__ == '__main__':
     unittest.main()
